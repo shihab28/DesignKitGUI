ca5825d7c88eabff2445c69abaf1c2e93e684cd9
shihab28 <shihab.ahamad28@gmail.com>
2022-04-16 15:51:18 +0600

    .

--- mainScript.py
+++ mainScript.py
@@ -26,6 +26,7 @@ class CREATE_CONFIG():
         self.switchSettings = ''
         self.outputFileName = ''
 
+        self.colorMainBg = "#F6F9F9"
         self.colorFrameBg = "#FFF6E9"
         self.colorHighlight = "#2A7FFF"
         self.colorActiveBg = "#FFF6E9"
@@ -35,7 +36,7 @@ class CREATE_CONFIG():
         self.colorActiveFg = "Black"
         self.colorInactiveFg = "Gray"
 
-        self.iconOpenFile = loadImage(f"{imageDir}/folderOpenGreen.png", (24,24))
+        self.iconOpenFile = loadImage(f"{imageDir}/folderOpenGreen.png", (20,20))
         self.mainFrameList = []
 
         self.createButtons()
@@ -71,14 +72,24 @@ class CREATE_CONFIG():
         
 
     def placeFrameCreateConfig(self):
-        relx=111; rely=181; relheight=180; relwidth=570; pad = 10
+        relx=111; rely=181; relwidth=570; relheight=180; pad = 10
         self.frameCreateConfigID = self.canvasFrame.create_window((relx+pad, rely+pad), anchor=NW, window=self.frameCreateConfig, tags="self.frameCreateConfig", width=relwidth-pad, height=relheight-pad)
         self.mainFrameList.append(self.frameCreateConfigID)
         
     
     def placeFrameLoadConfig(self):
-        relx=111; rely=181; relheight=180; relwidth=570; pad = 10
+        relx=111; rely=181; relwidth=570; relheight=180; pad = 10
         self.frameLoadConfigID = self.canvasFrame.create_window((relx+pad, rely+pad), anchor=NW, window=self.frameLoadConfig, tags="self.frameLoadConfig", width=relwidth-pad, height=relheight-pad)
+        self.mainFrameList.append(self.frameLoadConfigID)
+
+    def placeFrameForward(self):
+        relx=700; rely=225; relwidth=100; relheight=200; pad = 0
+        self.frameForwardID = self.canvasFrame.create_window((relx+pad, rely+pad), anchor=NW, window=self.frameForward, tags="self.frameForward", width=relwidth-pad, height=relheight-pad)
+        self.mainFrameList.append(self.frameLoadConfigID)
+
+    def placeFrameBackward(self):
+        relx=0; rely=225; relwidth=100; relheight=200; pad = 0
+        self.frameBackwardID = self.canvasFrame.create_window((relx+pad, rely+pad), anchor=NW, window=self.frameBackward, tags="self.frameBackward", width=relwidth-pad, height=relheight-pad)
         self.mainFrameList.append(self.frameLoadConfigID)
         
 
@@ -90,8 +101,30 @@ class CREATE_CONFIG():
         self.frameLoadConfig = Frame(self.canvasFrame, bg=self.colorFrameBg)
         self.placeFrameLoadConfig()
 
-        
+        # colorMainBg
+        self.frameForward = Frame(self.canvasFrame, bg=self.colorMainBg)
+        self.placeFrameForward()
+
+        self.iconForwardActive = loadImage(f"{imageDir}/forward_green_1024.png", (64,128))
+        self.iconForwardInactive = loadImage(f"{imageDir}/forward_gray_1024.png", (64,128))
+        self.forwardButton = Button(self.frameForward, bg = self.colorMainBg, \
+             highlightthickness=0, highlightbackground=self.colorMainBg, border=0, borderwidth=0)
+        self.forwardButton.config(image=self.iconForwardActive, state='normal')
+        self.forwardButton.config(image=self.iconForwardInactive, state='disabled')
+        self.forwardButton.pack(anchor=CENTER)  
+
+        self.frameBackward = Frame(self.canvasFrame, bg=self.colorMainBg)
+        self.placeFrameBackward()
+        self.iconBackwardActive = loadImage(f"{imageDir}/backward_green_1024.png", (64,128))
+        self.iconBackwardInactive = loadImage(f"{imageDir}/backward_gray_1024.png", (64,128))
+        self.backwardButton = Button(self.frameBackward, bg = self.colorMainBg, \
+             highlightthickness=0, highlightbackground=self.colorMainBg, border=0, borderwidth=0, disabledforeground=self.colorMainBg)
+        self.backwardButton.config(image=self.iconBackwardInactive, state='disabled')
+        # self.backwardButton.config(image=self.iconBackwardActive, state='normal')
+        self.backwardButton.pack(anchor=CENTER)
+        self.backwardButton.pack_forget()
         # self.colorFrameBg = "Yellow"
+
 
         self.frameLabelEntry = Frame(self.frameCreateConfig, bg=self.colorFrameBg, \
             highlightthickness=0, highlightbackground=self.colorFrameBg, border=0, borderwidth=0)
@@ -103,43 +136,42 @@ class CREATE_CONFIG():
         self.frameLabelEntry.columnconfigure(0, weight=2)
         self.frameLabelEntry.columnconfigure(1, weight=5)
         self.frameLabelEntry.columnconfigure(2, weight=1)
+        self.frameLabelEntry.rowconfigure(0, weight=1)
+        self.frameLabelEntry.rowconfigure(1, weight=1)
+        self.frameLabelEntry.rowconfigure(2, weight=1)
 
 
         self.labelLibraryPath = Label(self.frameLabelEntry, text='Library Path', bg=self.colorFrameBg, \
             highlightthickness=0, highlightbackground=self.colorActiveBg, border=0, borderwidth=0)
-        self.labelLibraryPath.grid(row=0, column=0, sticky=E, pady=10)
+        self.labelLibraryPath.grid(row=0, column=0, sticky=E, pady=(20, 0))
         self.entryLibraryPath = Entry(self.frameLabelEntry, width=70, highlightcolor=self.colorHighlight, highlightthickness=1)
-        self.entryLibraryPath.grid(row=0, column=1, padx=(5, 2),sticky=EW, pady=10)
+        self.entryLibraryPath.grid(row=0, column=1, padx=(5, 2),sticky=EW, pady=(20, 0))
         self.buttonLibraryPath = Button(self.frameLabelEntry, image=self.iconOpenFile, \
             highlightthickness=0, highlightbackground=self.colorActiveBg, border=0, borderwidth=0)
-        self.buttonLibraryPath.grid(row=0, column=2, sticky=W, pady=10)
+        self.buttonLibraryPath.grid(row=0, column=2, sticky=W, pady=(20, 0))
 
 
         self.labelSwitchSettings = Label(self.frameLabelEntry, text='Switch Settings', bg=self.colorFrameBg,  \
             highlightthickness=0, highlightbackground=self.colorFrameBg, border=0, borderwidth=0)
-        self.labelSwitchSettings.grid(row=1, column=0, sticky=E, pady=10)
+        self.labelSwitchSettings.grid(row=1, column=0, sticky=E, pady=(20, 0))
         self.entrySwitchSettings = Entry(self.frameLabelEntry, width=70, highlightcolor=self.colorHighlight, highlightthickness=1)
-        self.entrySwitchSettings.grid(row=1, column=1, padx=(5, 2),sticky=EW, pady=10)
+        self.entrySwitchSettings.grid(row=1, column=1, padx=(5, 2),sticky=EW, pady=(20, 0))
         self.buttonSwitchSettings = Button(self.frameLabelEntry, image=self.iconOpenFile, \
             highlightthickness=0, highlightbackground=self.colorActiveBg, border=0, borderwidth=0)
-        self.buttonSwitchSettings.grid(row=1, column=2, sticky=W, pady=10)
+        self.buttonSwitchSettings.grid(row=1, column=2, sticky=W, pady=(20, 0))
 
 
         self.labelOutputFileName = Label(self.frameLabelEntry, text='Output File Name', bg=self.colorFrameBg, \
             highlightthickness=0, highlightbackground=self.colorFrameBg, border=0, borderwidth=0)
-        self.labelOutputFileName.grid(row=2, column=0, sticky=E, pady=10)
+        self.labelOutputFileName.grid(row=2, column=0, sticky=E, pady=(20, 0))
         self.entryOutputFileName = Entry(self.frameLabelEntry, width=7, highlightcolor=self.colorHighlight, highlightthickness=1)
-        self.entryOutputFileName.grid(row=2, column=1, columnspan=2, padx=(5, 2), sticky=EW, pady=10)
+        self.entryOutputFileName.grid(row=2, column=1, columnspan=2, padx=(5, 2), sticky=EW, pady=(20, 0))
 
 
         self.buttonCreateCreateConfig = Button(self.frameButtonConfig, text="Create", bg=self.colorActibeButtonBg, fg=self.colorActibeButtonFg,  state='normal', \
              highlightthickness=0, highlightbackground=self.colorActibeButtonBg, border=0, borderwidth=0, font=("Helvetica", 12, font.BOLD))
-        self.buttonCreateCreateConfig.pack(expand=False, fill="x")
-
-        # self.frameCreateConfig.place_forget()
-        # self.canvasFrame.tag_raise()
-
-
+        self.buttonCreateCreateConfig.pack(expand=False, fill="x", pady=(20, 0))
+        
 
         
         self.frameLabelEntryLoad = Frame(self.frameLoadConfig, bg=self.colorFrameBg, \
@@ -160,7 +192,7 @@ class CREATE_CONFIG():
 
         self.labelConfigPath = Label(self.frameLabelEntryLoadOnly, text='Library Path', bg=self.colorFrameBg, \
             highlightthickness=0, highlightbackground=self.colorActiveBg, border=0, borderwidth=0)
-        self.labelConfigPath.grid(row=0, column=0, sticky=E, pady=10, padx=(32, 0))
+        self.labelConfigPath.grid(row=0, column=0, sticky=E, pady=10, padx=(33, 0))
         self.entryConfigPath = Entry(self.frameLabelEntryLoadOnly, width=71, highlightcolor=self.colorHighlight, highlightthickness=1)
         self.entryConfigPath.grid(row=0, column=1, padx=(5, 2),sticky=EW, pady=10)
         self.buttonConfigPath = Button(self.frameLabelEntryLoadOnly, image=self.iconOpenFile, \
@@ -179,18 +211,7 @@ class CREATE_CONFIG():
         
 
 
-        
-        
-        
-
-
-
-
-
-
-
 # defining color 
-
 colorMainBg = "#F6F9F9" # Gray
 colorMainBg1 = "#FFF6E9" # Ghiya
 colorMainFg = "Black"
